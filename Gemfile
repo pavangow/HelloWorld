8937da9d7485b19e0f8a1fb41bbaf3b29abf4f9e
pavan <pavanh2000@outlook.com>
2024-05-03 15:28:09 +0530

    final data

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-ruby "3.0.2"
+ruby "2.3.7"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3", ">= 7.1.3.2"
@@ -14,7 +14,7 @@ gem "sqlite3", "~> 1.4"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma"
 
-gem "capistrano"
+gem 'capistrano', '~> 3.0'
 
 gem "capistrano-rails"
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,54 +1,38 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.18.1"
+server '43.205.195.35', port: 80, roles: [:web, :app, :db], primary: true
 
-# Define your server and user
-server '13.60.64.135', user: 'ubuntu', roles: %w{web app db}
+set :repo_url,        'https://github.com/pavangow/HelloWorld.git'
+set :application,     'helloworld'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-# SSH options
-set :ssh_options, {
-  forward_agent: true,
-  auth_methods: %w[publickey],
-  keys: %w[/home/blubirch/Downloads/pavanpractice.pem]
-}
-
-set :stages, %w(production)
-
-# Application name
-set :application, "helloworld"
-
-# Git repository URL
-set :repo_url, 'https://github.com/pavangow/HelloWorld.git'
-
-# Deployment directory
-set :deploy_to, '/home/ubuntu/helloworld'
-
-# Use sudo for deployment (if needed)
-set :use_sudo, true
-
-# Default branch
-set :branch, 'main'
-
-# Linked files and directories
-set :linked_files, %w{config/master.key config/database.yml}
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Rails environment
-set :rails_env, 'production'
-
-# Number of releases to keep
-set :keep_releases, 2
-
-# Puma configuration
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-# Ensure required directories for Puma
+## Defaults:
+set :scm,           :git
+set :branch,        :main
+set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
@@ -57,6 +41,43 @@ namespace :puma do
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
+
   before :start, :make_dirs
 end
 
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/main`
+        puts "WARNING: HEAD is not the same as origin/main"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
